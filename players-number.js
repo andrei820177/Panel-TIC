50aa74d4e94b491ee38a8a355eae0a5e2c66f241
andrei352 <68961262+andrei820177@users.noreply.github.com>
2025-05-30 09:42:58 +0300

    players-number.js

--- players-number.js
+++ players-number.js
@@ -1,4 +1,4 @@
-fetch('/data.json')
+fetch('data.json')
         .then(response => response.json())
         .then(data => {
             document.getElementById('totalPlayers').textContent = data.totalPlayers;
